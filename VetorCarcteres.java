76845d53e9f0c78c1f49440ca47f7204353f62a7
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-04-07 09:15:32 -0300

    Update VetorCarcteres.java

--- VetorCarcteres.java
+++ VetorCarcteres.java
@@ -7,7 +7,7 @@ public class VetorCarcteres {
 
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    char caracteres[];
+    char[] caracteres;
     caracteres = new char[10];
     int numConsoantes = 0;
 
